fca4f4a79d698e2c3779e4f4fc4b8577cd36f2a2
Job Spijker <job.spijker@rivm.nl>
2019-10-09 09:42:28 +0200

    Added getobjlist function

---
+++ R/getobjlist.R
@@ -0,0 +1,33 @@
+#' Show datacube objects in database
+#'
+#' This function returns a data.frame with the name and description of all the objects and their
+#' automatically generated metadata
+#'
+#' @param schema database schema where the objects are stored
+#'
+#' @return data.frame with object names, description and automatically
+#' generated meta data
+#'
+#' @export
+
+
+
+getobjlist <- function(schema="datacube") {
+    x <- pgobjects::sql(paste("select robjects.name,key,value from ",schema,".robjects,",schema,".rkeyvalue 
+                              where robjects.did=rkeyvalue.did;",sep=""))
+
+    auditObjs <- x %>% filter(key=="audit") %>%
+        select(name)
+    
+    objs <- x %>% filter(!name%in%auditObjs$name)
+
+    dcKeys <- names(getkeyval())
+
+    objs <- objs %>% filter(key %in% c(dcKeys,"description")) %>% 
+        spread(key,value) %>%
+        na.omit() %>%
+        select(dcKeys,name,description)
+
+    return(objs)
+}
+
